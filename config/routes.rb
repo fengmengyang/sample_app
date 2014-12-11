f21cc5e324948684793e692e7ac2d5623a55c5e5
yang <fengmengyang@rd.tuan800.com>
2014-12-11 11:21:39 +0800

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
-  root to:'static_pages#home'
+  
+  resources :users
+  root to: 'static_pages#home'
   match '/home',to:'static_pages#home',via:'get'
   match '/help',to:'static_pages#help',via:'get'
   match '/contact',to:'static_pages#contact',via:'get'
---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :user do
+    name "yang"
+    email "yang@example.com"
+  end
+end
